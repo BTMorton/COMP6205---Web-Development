053b504cb143e6c02362d436eafd8ee1070bd71c
Ben Morton <ben@d1stkfactory>
2014-11-28 00:45:16 +0000

    Add progress bar animation and some nicer handling of things

--- index.php
+++ index.php
@@ -15,95 +15,92 @@
 			var peaks_inst;
 
 			function loadPeaks() {
-		    	requirejs.config({
+				requirejs.config({
 				  paths: {
-				    peaks: 'bower_components/peaks.js/src/main',
-				    EventEmitter: 'bower_components/eventemitter2/lib/eventemitter2',
-				    Kinetic: 'bower_components/kineticjs/kinetic',
-				    'waveform-data': 'bower_components/waveform-data/dist/waveform-data.min'
+					peaks: 'bower_components/peaks.js/src/main',
+					EventEmitter: 'bower_components/eventemitter2/lib/eventemitter2',
+					Kinetic: 'bower_components/kineticjs/kinetic',
+					'waveform-data': 'bower_components/waveform-data/dist/waveform-data.min'
 				  }
 				});
 
 				// requires it
 				require(['peaks'], function (Peaks) {
 				  peaks_inst = Peaks.init({
-				    container: document.querySelector('#peaks_container'),
-				    mediaElement: document.querySelector('#peaks_player'),
-				    height: 200,
-				    zoomLevels: [512, 1024, 2048, 4096],
-				    keyboard: true,
-				    overviewWaveformColor: "#d1e751"
+					container: document.querySelector('#peaks_container'),
+					mediaElement: document.querySelector('#peaks_player'),
+					height: 200,
+					zoomLevels: [512, 1024, 2048, 4096],
+					keyboard: true,
+					overviewWaveformColor: "#d1e751"
 				  });
 
 				  peaks_inst.on('segments.ready', function(){
-				  	$("#progress").addClass("hidden");
-				  	$("#peaks_container").removeAttr("style");
-				    // do something when segments are ready to be displayed
+					$("#progress").addClass("hidden");
+					$("#peaks_container").removeAttr("style");
+					// do something when segments are ready to be displayed
 				  });
 				});
 			}
 
 			$(function() {
 				$(window).scroll(function() {
-				    if ($(window).scrollTop() >= 165) {
-				       $('header').addClass('fixed');
-				    } else {
-				       $('header').removeClass('fixed');
-				    }
+					if ($(window).scrollTop() >= 165) {
+					   $('header').addClass('fixed');
+					} else {
+					   $('header').removeClass('fixed');
+					}
 
-				    if ($(window).scrollTop() > ($("#about").offset().top - $("#about h2").height())) {
-				    	$("header .nav a:not(.about).active").removeClass("active");
-				    	$("header .nav .about").addClass("active");
-				    } else if ($(window).scrollTop() > ($("#tutorials").offset().top - $("#tutorials h2").height())) {
-				    	$("header .nav a:not(.tuts).active").removeClass("active");
-				    	$("header .nav .tuts").addClass("active");
-				    } else {
-				    	$("header .nav a:not(.edit).active").removeClass("active");
-				    	$("header .nav .edit").addClass("active");
-				    }
+					if ($(window).scrollTop() > ($("#about").offset().top - $("#about h2").height())) {
+						$("header .nav a:not(.about).active").removeClass("active");
+						$("header .nav .about").addClass("active");
+					} else if ($(window).scrollTop() > ($("#tutorials").offset().top - $("#tutorials h2").height())) {
+						$("header .nav a:not(.tuts).active").removeClass("active");
+						$("header .nav .tuts").addClass("active");
+					} else {
+						$("header .nav a:not(.edit).active").removeClass("active");
+						$("header .nav .edit").addClass("active");
+					}
 				});
 
-			    $('#file_upload').fileupload({
-			        url: './upload.php',
-			        dataType: 'json',
-			        done: function (e, data) {
-			        	console.log(data.result);
-			        	if (data.result.file_name != "" && data.result.file_name != undefined) {
-			        		$("#peaks_container").removeClass("hidden").css("height", '0').css('opacity', '0');
-			        		$("#progress").text("Uploaded. Generating waveform...");;
-				        	$("#peaks_player source").attr("src", data.result.file_name);
-				        	$("#peaks_player")[0].load();
-				        	loadPeaks();
-				        } else {
-				        	$("#progress").addClass("error").text("Error: "+data.result.error)
-				        }
-			        },
-			        progressall: function (e, data) {
-			            var progress = parseInt(data.loaded / data.total * 100, 10);
-			            var box = $("#progress");
-
-			            if (box.hasClass("hidden")) {
-			            	$("#placeholder").addClass("hidden");
-			            	box.removeClass("hidden");
-			            } else if (box.hasClass("error")) {
-			            	box.removeClass("error");
-			            }
-
-			            box.text("Progress: " + progress + "%");
-			            $("#progress:before").width(progress+"%");
-			    		console.log("Progress: "+progress);
-			        }
-			    }).prop('disabled', !$.support.fileInput)
-			        .parent().addClass($.support.fileInput ? undefined : 'disabled');
-			    $("#play_button").click(function() {
-			    	if ($(this).hasClass("pause")) {
-			    		$(this).removeClass("pause");
-			    		$("#peaks_player")[0].pause();
-			    	} else {
-			    		$(this).addClass("pause");
-			    		$("#peaks_player")[0].play();
-			    	}
-			    })
+				$('#file_upload').fileupload({
+					url: './upload.php',
+					dataType: 'json',
+					start: function (e, data) {
+						$("#peaks_container").addClass("hidden");
+						$("#placeholder").addClass("hidden");
+						$("#progress").removeClass("hidden").removeClass("error");
+					},
+					done: function (e, data) {
+						console.log(data.result);
+						if (data.result.file_name != "" && data.result.file_name != undefined) {
+							$("#peaks_container").removeClass("hidden").css("height", '0').css('opacity', '0');
+							$("#progress_text").text("Uploaded. Generating waveform...");;
+							$("#peaks_player source").attr("src", data.result.file_name);
+							$("#peaks_player")[0].load();
+							loadPeaks();
+						} else {
+							$("#progress").addClass("error");
+							$("#progress_text").text("Error: "+data.result.error)
+						}
+					},
+					progressall: function (e, data) {
+						var progress = parseInt(data.loaded / data.total * 100, 10);
+						$("#progress_text").text("Progress: " + progress + "%");
+						$("#progress_bar").width(progress+"%");
+						console.log("Progress: "+progress);
+					}
+				}).prop('disabled', !$.support.fileInput)
+					.parent().addClass($.support.fileInput ? undefined : 'disabled');
+				$("#play_button").click(function() {
+					if ($(this).hasClass("pause")) {
+						$(this).removeClass("pause");
+						$("#peaks_player")[0].pause();
+					} else {
+						$(this).addClass("pause");
+						$("#peaks_player")[0].play();
+					}
+				})
 			});
 		</script>
 	</head>
@@ -126,7 +123,10 @@
 				<div id="placeholder">
 					Use the uploader above to upload your file and begin editing!
 				</div>
-				<div id="progress" class="hidden"></div>
+				<div id="progress" class="hidden">
+					<div id="progress_bar"></div>
+					<div id="progress_text"></div>
+				</div>
 				<div id="peaks_container" class="hidden"></div>
 				<audio id="peaks_player">
 					<source src="007384.mp3" type="audio/mp3" />
